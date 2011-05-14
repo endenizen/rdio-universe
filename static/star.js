199c3ea4251d08c1db207e3d76e74f9731d3ad9f
Peter van Hardenberg <pvh@heroku.com>
2011-05-14 13:35:43 -0700

    move shaders out of init so they can be shared

--- static/star.js
+++ static/star.js
@@ -8,103 +8,41 @@ function Star(universe, scene, obj) {
   this.init();
 }
 
-Star.prototype.init = function() {
 
-  var Shaders = {
-    'earth' : {
-      uniforms: {
-        'texture': { type: 't', value: 0, texture: null },
-        'time': { type: 'f', value: 1.0 },
-        'alpha': { type: 'f', value: 0 }
-      },
-      vertexShader: [
-        'varying vec3 vNormal;',
-        'varying vec2 vUv;',
-        'void main() {',
-          'gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );',
-          'vNormal = normalize( normalMatrix * normal );',
-          'vUv = uv;',
-        '}'
-      ].join('\n'),
-      fragmentShader: [
-        'uniform sampler2D texture;',
-        'varying vec3 vNormal;',
-        'varying vec2 vUv;',
-        'void main() {',
-          'vec3 diffuse = texture2D( texture, vUv ).xyz;',
-          'float intensity = 1.05 - dot( vNormal, vec3( 0.0, 0.0, 1.0 ) );',
-          'vec3 atmosphere = vec3( 1.0, 1.0, 1.0 ) * pow( intensity, 3.0 );',
-          'gl_FragColor = vec4( diffuse + atmosphere, 1.0 );',
-        '}'
-      ].join('\n'),
-      fragmentShaderGlowy: [
-        'uniform sampler2D texture;',
-        'uniform float alpha;',
-        'uniform float time;',
-        'varying vec3 vNormal;',
-        'varying vec2 vUv;',
-        'vec3 noise() {',
-          'return vec3(1.2,3.3,0.5);',
-          'vec2 resolution = vec2( 200.0, 200.0 );',
-          'vec2 p = -1.0 + 2.0 * vUv;',
-          'float a = time*40.0;',
-          'float d,e,f,g=1.0/40.0,h,i,r,q;',
-          'e=400.0*(p.x*0.5+0.5);',
-          'f=400.0*(p.y*0.5+0.5);',
-          'i=200.0+sin(e*g+a/150.0)*20.0;',
-          'd=200.0+cos(f*g/2.0)*18.0+cos(e*g)*7.0;',
-          'r=sqrt(pow(i-e,2.0)+pow(d-f,2.0));',
-          'q=f/r;',
-          'e=(r*cos(q))-a/2.0;f=(r*sin(q))-a/2.0;',
-          'd=sin(e*g)*176.0+sin(e*g)*164.0+r;',
-          'h=((f+d)+a/2.0)*g;',
-          'i=cos(h+r*p.x/1.3)*(e+e+a)+cos(q*g*6.0)*(r+h/3.0);',
-          'h=sin(f*g)*144.0-sin(e*g)*212.0*p.x;',
-          'h=(h+(f-e)*q+sin(r-(a+h)/7.0)*10.0+i/4.0)*g;',
-          'i+=cos(h*2.3*sin(a/350.0-q))*184.0*sin(q-(r*4.3+a/12.0)*g)+tan(r*g+h)*184.0*cos(r*g+h);',
-          'i=mod(i/5.6,256.0)/64.0;',
-          'if(i<0.0) i+=4.0;',
-          'if(i>=2.0) i=4.0-i;',
-          'd=r/350.0;',
-          'd+=sin(d*d*8.0)*0.52;',
-          'f=(sin(a*g)+1.0)/2.0;',
-          'return vec3(f*i/1.6,i/2.0+d/13.0,i)*d*p.x + vec3(i/1.3+d/8.0,i/2.0+d/18.0,i)*d*(1.0-p.x);',
-        '}',
-        'void main() {',
-          'vec3 diffuse = texture2D( texture, vUv ).xyz;',
-          'float intensity = 1.05 - dot( vNormal, vec3( 0.0, 0.0, 1.0 ) );',
-          'vec3 atmosphere = vec3( 1.0, 1.0, 1.0 ) * pow( intensity, 3.0 );',
-          'vec3 noise = noise() * 0.5;',
-
-          //float n = snoise(vec4(4.0 * v_texCoord3D.xyz, 0.5 * time));
-          //gl_FragColor = v_color * vec4(0.5 + 0.5 * vec3(n, n, n), 1.0);
-
-          'gl_FragColor = vec4( diffuse + atmosphere + noise, alpha );',
-        '}'
-      ].join('\n'),
+Star.prototype.Shaders = {
+  'earth' : {
+    uniforms: {
+      'texture': { type: 't', value: 0, texture: null },
+      'time': { type: 'f', value: 1.0 },
+      'alpha': { type: 'f', value: 0 }
     },
-    'atmosphere' : {
-      uniforms: { 'alpha': { type: 'f', value: 0 }
-      },
-      vertexShader: [
-        'varying vec3 vNormal;',
-        'void main() {',
-          'vNormal = normalize( normalMatrix * normal );',
-          'gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );',
-        '}'
-      ].join('\n'),
-      fragmentShader: [
-        'varying vec3 vNormal;',
-        'void main() {',
-          'float intensity = pow( 0.8 - dot( vNormal, vec3( 0, 0, 1.0 ) ), 12.0 );',
-          'gl_FragColor = vec4( 1.0, 1.0, 1.0, alpha ) * intensity;',
-        '}'
-      ].join('\n')
-    }
-  };
+    vertexShader: [
+      'varying vec3 vNormal;',
+      'varying vec2 vUv;',
+      'void main() {',
+        'gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );',
+        'vNormal = normalize( normalMatrix * normal );',
+        'vUv = uv;',
+      '}'
+    ].join('\n'),
+    fragmentShader: [
+      'uniform sampler2D texture;',
+      'varying vec3 vNormal;',
+      'varying vec2 vUv;',
+      'void main() {',
+        'vec3 diffuse = texture2D( texture, vUv ).xyz;',
+        'float intensity = 1.05 - dot( vNormal, vec3( 0.0, 0.0, 1.0 ) );',
+        'vec3 atmosphere = vec3( 1.0, 1.0, 1.0 ) * pow( intensity, 3.0 );',
+        'gl_FragColor = vec4( diffuse + atmosphere, 1.0 );',
+      '}'
+    ].join('\n')
+  }
+};
 
+Star.prototype.init = function() {
   var geometry = new THREE.Sphere(100, 40, 30);
-  var shader = Shaders['earth'];
+
+  var shader = this.Shaders['earth'];
   var uniforms = THREE.UniformsUtils.clone(shader.uniforms);
   uniforms['texture'].texture = THREE.ImageUtils.loadTexture(this.obj.icon);
 
